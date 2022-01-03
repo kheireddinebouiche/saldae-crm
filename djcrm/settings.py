b42395f7bb795c97d00cdbdd7124c6db1648314f
kheireddinebouiche <40995455+kheireddinebouiche@users.noreply.github.com>
2022-01-03 10:07:19 +0100

    Update settings.py

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '0yino@3!_ta-#kcoj(d!aqif9&g(&a6=__(z=^$w!=1+-_-u$s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
